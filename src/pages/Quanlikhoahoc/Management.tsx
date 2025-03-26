17e10a85d8436b554906a41888a301b154a64182
maibui7879 <maibui7879@gmail.com>
2025-03-26 14:32:47 +0700

    gae

--- src/pages/Quanlikhoahoc/Management.tsx
+++ src/pages/Quanlikhoahoc/Management.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Button } from "antd";
 import { ReadOutlined, TeamOutlined } from "@ant-design/icons";
-import { motion } from "framer-motion";
 import TeacherList from "./TeacherList";
 import CourseList from "./CourseList";
 
@@ -11,23 +10,13 @@ const Management = () => {
   return (
     <div>
       <div style={{ textAlign: "center", marginBottom: "16px" }}>
-        <h1 style={{ fontSize: "40px" }}>
-          {view === "teachers" ? " Danh sách Giảng viên" : " Danh sách Khóa học"}
-        </h1>
+        <h1 style={{ fontSize: "40px"}}>{view === "teachers" ? " Danh sách Giảng viên" : " Danh sách Khóa học"} </h1>
         <Button type="default" onClick={() => setView(view === "teachers" ? "courses" : "teachers")}>
           {view === "teachers" ? <ReadOutlined /> : <TeamOutlined />}
           {view === "teachers" ? " Xem danh sách khóa học" : " Xem danh sách giảng viên"}
         </Button>
       </div>
-      <motion.div
-        key={view}
-        initial={{ x: "100%", opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        exit={{ x: "-100%", opacity: 0 }}
-        transition={{ duration: 0.5, ease: "easeInOut" }}
-      >
-        {view === "teachers" ? <TeacherList /> : <CourseList />}
-      </motion.div>
+      {view === "teachers" ? <TeacherList /> : <CourseList />}
     </div>
   );
 };
